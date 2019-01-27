4dac20370a786ba26aefaf9e8e7c05eb7e4a6e1b
asheraryam <asheraryam@gmail.com>
2019-01-27 10:45:23 +0200

    Added grey overlay effect for rain.

--- game/Autoload/WorldHelper.gd
+++ game/Autoload/WorldHelper.gd
@@ -14,10 +14,14 @@ func _ready():
 
 func set_rain(value):
 	if value:
+		if not world.get_node("Weather/Rain").emitting:
+			world.get_node("Weather/GreyEffect/AnimationPlayer").play("start")	
 		world.get_node("Backgrounds/Sky").animation = "dark"
 		world.get_node("Weather/Rain").emitting = true
 		world.get_node("SFX")._fade_in(world.get_node("SFX/rain"))
 	else:
+		if world.get_node("Weather/Rain").emitting:
+			world.get_node("Weather/GreyEffect/AnimationPlayer").play("end")
 		world.get_node("Backgrounds/Sky").animation = "light"
 		world.get_node("Weather/Rain").emitting = false
 		world.get_node("SFX")._fade_out(world.get_node("SFX/rain"))
