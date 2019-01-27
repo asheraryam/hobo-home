e105f16e4d081f6059da7c5134a10663b2d6b9ff
asheraryam <asheraryam@gmail.com>
2019-01-27 14:38:34 +0200

    Hobo now uses the object's specified "usage animation" when he reaches it.

--- game/World/Hobo.gd
+++ game/World/Hobo.gd
@@ -6,6 +6,7 @@ var item_target = null
 var item_in_use = null
 
 var moving = false
+var last_used = null
 
 func _ready():
 	randomize()
@@ -31,10 +32,10 @@ func _on_next_desire_timeout():
 	move_to(Vector2(item_target.position.x, position.y))
 	
 func valid_desire_object(item):
-	return item and item.has_method("set_desired") and abs(item.position.y - position.y) < 200 and not item.being_displayed and not WorldHelper.pressed_object == item and not item.dead
+	return item and item != last_used and item.has_method("set_desired") and abs(item.position.y - position.y) < 200 and not item.being_displayed and not WorldHelper.pressed_object == item and not item.dead
 
 func move_to(pos):
-	$icon.play("walking")
+	$icon.play("Walk")
 	$icon.flip_h = position.x - pos.x < 0
 	var walk_duration = abs(position.x - pos.x)/100
 	$walk_tween.interpolate_property(self, "position",
@@ -45,24 +46,36 @@ func move_to(pos):
 	
 	$moving.wait_time = walk_duration
 	$moving.start()
-	$moving.connect("timeout", self, "target_reached", [item_target.name])
+	$moving.connect("timeout", self, "target_reached", [item_target.name, item_target])
 
-func target_reached(thing):
-	$icon.play("default")
-	moving = false
+func target_reached(thing_name, thing):
 	var time_to_next = default_action_time
-	if item_target and item_target.name == thing:
+	if item_target:
+		$icon.play(thing.usage_animation)
 		if item_target.time_for_activity != -1:
 			time_to_next = item_target.time_for_activity
 		item_target.activate_used(default_action_time)
-		$next_desire.wait_time = time_to_next
-		$next_desire.start()
+		$usage_timer.wait_time = time_to_next
+		$usage_timer.connect("timeout", self, "idling", [item_target])
+		$usage_timer.start()
+#		$next_desire.wait_time = time_to_next
+		$next_desire.stop()
 	else:
+#		last_used = null
 		item_target = null
+		moving = false
 #		return
 		_on_next_desire_timeout()
 
+func idling(last_item):
+	moving = false
+	last_used = last_item
+	$icon.play("default")
+	$next_desire.wait_time = (randi() % 5) + 2
+	$next_desire.start()
+
 func target_interrupted():
+	last_used = null
 	$icon.play("default")
 	$moving.disconnect("timeout", self, "target_reached")
 	moving = false
